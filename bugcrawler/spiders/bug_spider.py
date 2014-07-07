e2e9fa0b8b1f29004514bdce66af353536fce729
XiaoYong Yuan <chbrian77@gmail.com>
2014-07-08 02:24:22 +0800

    change spider

--- bugcrawler/spiders/bug_spider.py
+++ bugcrawler/spiders/bug_spider.py
@@ -1,14 +1,24 @@
 import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
 
 
 class BugSpider(scrapy.Spider):
     name = "bug_crawler"
     allowed_domains = ["launchpad.net"]
     start_urls = [
-        "https://bugs.launchpad.net/openstack/",
+        "https://bugs.launchpad.net/openstack/"
     ]
 
-    def parse(self, response):
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+    rules = (
+        Rule(LinkExtractor(allow=('https://bugs.launchpad.net/[a-z]*/+bug/[0-9]+')))
+    )
+
+    def parse_item(self, response):
+        self.log('Hi, this is an item page! %s' % response.url)
+
+        item = scrapy.Item()
+        item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+        item['name'] = response.xpath('//td[@id="item_name"]/text()').extract()
+        item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
+        return item
