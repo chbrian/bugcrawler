3b5c54eb727bf940a845c7b4e876730a0e1cd787
XiaoYong Yuan <chbrian77@gmail.com>
2014-07-08 02:54:01 +0800

    fix bugs

--- bugcrawler/spiders/bug_spider.py
+++ bugcrawler/spiders/bug_spider.py
@@ -1,7 +1,8 @@
 import scrapy
+import bugcrawler.items as items
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
-
+from scrapy.selector import HtmlXPathSelector
 
 class BugSpider(scrapy.Spider):
     name = "bug_crawler"
@@ -11,17 +12,15 @@ class BugSpider(scrapy.Spider):
     ]
 
     rules = (
-        Rule(LinkExtractor(allow=('https://bugs.launchpad.net/[a-z]*/+bug/[0-9]+')), callback=None, follow=True)
+        Rule(LinkExtractor(allow=('https://bugs.launchpad.net/[a-z]*/+bug/[0-9]+')), callback=parse_item, follow=True)
     )
-
-    """
 
     def parse_item(self, response):
         self.log('Hi, this is an item page! %s' % response.url)
-
-        item = scrapy.Item()
-        item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
-        item['name'] = response.xpath('//td[@id="item_name"]/text()').extract()
-        item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
-        return item
-    """
+        hxs = HtmlXPathSelector(response)
+        if response.url:
+            item = items.BugcrawlerItem()
+            item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+            item['name'] = response.xpath('//td[@id="item_name"]/text()').extract()
+            item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
+            yield item
